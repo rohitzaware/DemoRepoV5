f0b5076827e34c2ed70f5a77c48fbca070b052a8
rohitzaware <rohitzaware999@gmail.com>
2023-04-09 12:10:01 +0530

    Basic Task Commit

---
+++ TestProject/src/testPackage/TestClass.java
@@ -0,0 +1,15 @@
+package testPackage;
+
+public class TestClass {
+
+	public static void main(String arg []) {
+    
+		System.out.println("TestBase class");
+		 System.out.println("Util class");
+		 System.out.println("PageLayer class");
+		 System.out.println("TestLayer class");
+
+
+
+	}
+}
